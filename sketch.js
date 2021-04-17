743720d3cf31da732c4ae81afe60ad177fcbd07f
Ritu2009 <73284797+Ritu2009@users.noreply.github.com>
2021-04-17 15:28:03 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,52 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+var thunder, thunder1,thunder2,thunder3,thunder4;
+
+var engine, world;
+var drops = [];
+var rand;
+
+var maxDrops=100;
+
+var thunderCreatedFrame=0;
+
+function preload(){
+    thunder1 = loadImage("thunderbolt/1.png");
+    thunder2 = loadImage("thunderbolt/2.png");
+    thunder3 = loadImage("thunderbolt/3.png");
+    thunder4 = loadImage("thunderbolt/4.png");
+}
+
+function setup(){
+    engine = Engine.create();
+    world = engine.world;
+
+    createCanvas(400,700);
+    
+
+    //creating drops
+    if(frameCount % 150 === 0){
+
+        for(var i=0; i<maxDrops; i++){
+            drops.push(new Drop(random(0,400), random(0,400)));
+        }
+
+    }
+    
+}
+
+function draw(){
+    Engine.update(engine);
+    background(0); 
+
+    //displaying rain drops
+    for(var i = 0; i<maxDrops; i++){
+        drops[i].showDrop();
+        drops[i].updateY()
+        
+    }
+
+    drawSprites();
+}   
+
